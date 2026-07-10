9dd74f70ac007fb25941ad6b3ca5aa7eefb2467d
Bahar Büşra Atasoy <baharatasoy280@gmail.com>
2026-07-10 17:39:30 +0300

    Ders3 - Pratikler

---
+++ src/ders3/Pratikler/D134_Pratik.kt
@@ -0,0 +1,14 @@
+package ders3.Pratikler
+
+fun main() {
+
+    print("Adınızı giriniz:")
+    val isim = readln()
+
+    print("Yaşınızı giriniz:")
+    val yas = readln().toInt()
+
+    println("Merhaba\uD83D\uDC4B $isim demek $yas yaşındasın  ")
+
+
+}
---
+++ src/ders3/Pratikler/D135_RolYapmaOyunu.kt
@@ -0,0 +1,27 @@
+package ders3.Pratikler
+
+fun main() {
+
+    var komutanCani =100
+    println("\uD83E\uDE96Komutan canı: $komutanCani")
+    var dusmanCani = 100
+    println("\uD83E\uDDDFDüşman canı: $dusmanCani")
+
+    val hasar = (15..35).random()
+
+    println(" Verdiğin Hasar: $hasar")
+
+    dusmanCani -= hasar
+
+    println("\uD83E\uDE78 Düşman yaralandı! Kalan canı: $dusmanCani")
+
+    val aldiginHasar = (15..35).random()
+
+    komutanCani -= aldiginHasar
+    println("Aldığın Hasar: $aldiginHasar")
+    println("⚔\uFE0F Düşman atağa geçti! Kalan canın: $komutanCani")
+
+
+
+
+}
