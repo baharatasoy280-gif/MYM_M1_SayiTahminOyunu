b4d31daa289daf70facaa2c2b3eaec3c9963bcbf
Bahar Büşra Atasoy <baharatasoy280@gmail.com>
2026-07-10 19:22:24 +0300

    Ders3 - Pratikler

---
+++ src/ders3/Pratikler/D135_KisiselSaglikRaporu.kt
@@ -0,0 +1,34 @@
+package ders3.Pratikler
+
+fun main() {
+
+    print("Lütfen adınızı giriniz:")
+
+    val ad = readln()
+
+    print("Boyunuzu giriniz:")
+    val boy = readln().toDouble()
+
+    print("Kilonuzu giriniz:")
+    val kilo = readln().toDouble()
+
+    val vki = kilo / (boy * boy)
+
+    val formatliVki = "%.2f".format(vki)
+
+    print("Sayın $ad, Vücut kitle indeksiniz: $formatliVki ")
+
+
+    println(
+        """
+           18,5 ve altı: Zayıf
+           18,5 - 24,9: Normal / İdeal kilo
+           25,0 - 29,9: Fazla kilolu
+           30,0 - 34,9: 1. Derece Obezite
+           35,0 - 39,9: 2. Derece Obezite
+           40,0 ve üzeri: 3. Derece Obezite (Morbid Obezite) 
+       """.trimIndent()//trimIndent() soldaki boşlukları otomatik siler
+    )
+
+
+}
